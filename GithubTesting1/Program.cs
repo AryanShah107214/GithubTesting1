99fa3bdcedcb1e534e859f0eca9275bc1ed0004b
Aryan Shah <ac107214@avcol.school.nz>
2020-05-22 14:48:10 +1200

    Add project files.

---
+++ GithubTesting1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubTesting1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
